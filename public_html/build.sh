7cdd19447a7f44284dee0ab01514953b7674b093
Nishant Jha <ninjha01@gmail.com>
2017-05-30 16:01:53 -0400

    build.sh also checks essays directory

--- public_html/build.sh
+++ public_html/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 for file in $(ls *.md);
-do pandoc --ascii -f markdown -t html "${file}" -o "${file%md}html";
+do pandoc --ascii -f markdown -t html "${file}" -o "${file%md}html"; mv "${file%md}html" ../myweb/html/;
 done;
 
-mv *.html ../myweb/html/;
+for file in $(ls essays/*.md);
+do pandoc --ascii -f markdown -t html "${file}" -o "${file%md}html"; mv "${file%md}html" ../myweb/html/;
+done;
+
+#mv *.html ../myweb/html/;
