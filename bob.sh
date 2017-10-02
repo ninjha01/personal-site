ca27558a1d1195e203c29e39a33afa6922482e14
Nishant Jha <ninjha01@gmail.com>
2017-10-02 10:52:41 -0400

    modularizing

--- bob.sh
+++ bob.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
 
+quoteSubst() {
+  IFS= read -d '' -r < <(sed -e ':a' -e '$!{N;ba' -e '}' -e 's/[&/\]/\\&/g; s/\n/\\&/g' <<<"$1")
+  printf %s "${REPLY%$'\n'}"
+}
+
 #Build External Pages
 for file in $(ls ./public_html/content/essays/*.md);
 do
-    #Build the top
-    cat ./public_html/boilerplate/top_generic.html > "${file%md}html";
-    #Build the content
-    pandoc --ascii -f markdown -t html "${file}" >> "${file%md}html";
+    
+    TAG="{CONTENT}";
+    VALUE=$(pandoc --ascii -f markdown -t html "${file}");
+    sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" ./public_html/boilerplate/generic.html > "${file%md}html";
+    
     ##Build the Menu
     cd ./myweb/html/;
-    ../../public_html/builders/menu_builder.out ../../public_html/content/essays/writing.md  ../../public_html/content/projects.md >> "../../${file%md}html";
+    TAG="{MENU}";
+    VALUE=$(../../public_html/builders/menu_builder.out ../../public_html/content/essays/writing.md ../../public_html/content/projects.md);
     cd ../../;
-    #Build the bottom
-    cat ./public_html/boilerplate/bottom_generic.html >> "${file%md}html";
-    mv "${file%md}html" ./myweb/html/;
+    sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" ${file%md}html > "${file%md}html";
 done;
 
 #Build Home Page
 
-##Build the Top
-rm ./myweb/html/index.html;
-touch ./myweb/html/index.html;
-cat ./public_html/boilerplate/top_home.html > ./myweb/html/index.html
+
 ##Build Projects
-./public_html/builders/project_builder.out ./public_html/content/projects.md >> ./myweb/html/index.html
+TAG="{PROJECTS}";
+VALUE=$(./public_html/builders/project_builder.out ./public_html/content/projects.md);
+sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" ./public_html/boilerplate/home.html > ./myweb/html/index.html;
+
 ##Build Writing
-cd ./myweb/html/;
-../../public_html/builders/writing_builder.out ../../public_html/content/essays/writing.md >> ../../myweb/html/index.html
-cd ../../;
+TAG="{WRITING}";
+VALUE=$(./public_html/builders/writing_builder.out ./public_html/content/writing.md);
+sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" ./myweb/html/index.html > ./myweb/html/index.html;
+
 ##Build the Menu
-cd ./myweb/html/;
-../../public_html/builders/menu_builder.out ../../public_html/content/essays/writing.md  ../../public_html/content/projects.md >> ../../myweb/html/index.html;
-cd ../../;
-##Build the Bottom
-cat ./public_html/boilerplate/bottom_home.html >> ./myweb/html/index.html;
+TAG="{MENU}";
+VALUE=$(./public_html/builders/menu_builder.out ./public_html/content/writing.md);
+sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" ./myweb/html/index.html > ./myweb/html/index.html;
--- init.sh
+++ init.sh
@@ -1,15 +1,8 @@
-filename='init.md'
+filename='details.md'
 while read p; do
     
     TAG=$(echo $p | cut -f1 -d-);
     VALUE=$(echo $p | cut -f2 -d-);
-    echo $TAG;
-    echo $VALUE;
-    
-    sed -i -e "s/$TAG/$VALUE/g" public_html/boilerplate/top_generic.html;
-    sed -i -e "s/$TAG/$VALUE/g" public_html/boilerplate/top_home.html;
-    sed -i -e "s/$TAG/$VALUE/g" public_html/boilerplate/bottom_generic.html;
-    sed -i -e "s/$TAG/$VALUE/g" public_html/boilerplate/bottom_home.html;
+    sed -e "s/$TAG/$VALUE/g" ./public_html/boilerplate/template/generic_template.html;
+    sed -e "s/$TAG/$VALUE/g" ./public_html/boilerplate/template/home_template.html > ./public_html/boilerplate/home.html; 
 done < $filename
-
-
