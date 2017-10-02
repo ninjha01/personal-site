b4a8105799a00098435764c289f1541027a023c3
Nishant Jha <ninjha01@gmail.com>
2017-10-02 12:11:12 -0400

    finalized templating process

--- bob.sh
+++ bob.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
 quoteSubst() {
-  IFS= read -d '' -r < <(sed -e ':a' -e '$!{N;ba' -e '}' -e 's/[&/\]/\\&/g; s/\n/\\&/g' <<<"$1")
-  printf %s "${REPLY%$'\n'}"
+    IFS= read -d '' -r < <(sed -e ':a' -e '$!{N;ba' -e '}' -e 's/[&/\]/\\&/g; s/\n/\\&/g' <<<"$1")
+    printf %s "${REPLY%$'\n'}"
 }
 
 #Build External Pages
-for file in $(ls ./public_html/content/essays/*.md);
+cd ./public_html/content/essays/;
+for file in $(ls *.md);
 do
-    
     TAG="{CONTENT}";
     VALUE=$(pandoc --ascii -f markdown -t html "${file}");
-    sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" ./public_html/boilerplate/generic.html > "${file%md}html";
+    sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" ../../boilerplate/generic.html > temp.html;
+    mv temp.html "../../../myweb/html/${file%md}html"; 
     
     ##Build the Menu
-    cd ./myweb/html/;
+    cd ../../../myweb/html/;
     TAG="{MENU}";
     VALUE=$(../../public_html/builders/menu_builder.out ../../public_html/content/essays/writing.md ../../public_html/content/projects.md);
-    cd ../../;
-    sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" ${file%md}html > "${file%md}html";
+    cd ../../public_html/content/essays/;
+    sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" "../../../myweb/html/${file%md}html" > temp.html;
+    mv temp.html "../../../myweb/html/${file%md}html";
+
+    
 done;
-
+cd ../../../;
 #Build Home Page
-
 
 ##Build Projects
 TAG="{PROJECTS}";
@@ -31,10 +34,12 @@ sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" ./public_htm
 
 ##Build Writing
 TAG="{WRITING}";
-VALUE=$(./public_html/builders/writing_builder.out ./public_html/content/writing.md);
-sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" ./myweb/html/index.html > ./myweb/html/index.html;
+VALUE=$(./public_html/builders/writing_builder.out ./public_html/content/essays/writing.md);
+sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" ./myweb/html/index.html > temp.html;
+mv temp.html ./myweb/html/index.html;
 
 ##Build the Menu
 TAG="{MENU}";
-VALUE=$(./public_html/builders/menu_builder.out ./public_html/content/writing.md);
-sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" ./myweb/html/index.html > ./myweb/html/index.html;
+VALUE=$(./public_html/builders/menu_builder.out ./public_html/content/essays/writing.md ./public_html/content/projects.md);
+sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/" ./myweb/html/index.html > temp.html;
+mv temp.html ./myweb/html/index.html;
--- init.sh
+++ init.sh
@@ -1,8 +1,17 @@
 filename='details.md'
+
+cp ./public_html/boilerplate/template/generic_template.html ./public_html/boilerplate/generic.html;
+cp ./public_html/boilerplate/template/home_template.html ./public_html/boilerplate/home.html;
+
 while read p; do
     
     TAG=$(echo $p | cut -f1 -d-);
     VALUE=$(echo $p | cut -f2 -d-);
-    sed -e "s/$TAG/$VALUE/g" ./public_html/boilerplate/template/generic_template.html;
-    sed -e "s/$TAG/$VALUE/g" ./public_html/boilerplate/template/home_template.html > ./public_html/boilerplate/home.html; 
+    sed -i -e "s/$TAG/$VALUE/g" public_html/boilerplate/generic.html;
+    sed -i -e "s/$TAG/$VALUE/g" public_html/boilerplate/home.html;
+
+    ##Don't know why I need to do this
+    rm public_html/boilerplate/*.html-e;
 done < $filename
+
+
