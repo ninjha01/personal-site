45f80b734b3908cbbaba5f70df4a8dc0f65ea5dc
Nishant Jha <ninjha01@gmail.com>
2017-10-02 17:08:22 -0400

    removed necessity for manual escaping

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 filename='details.md'
 
+quoteSubst() {
+    IFS= read -d '' -r < <(sed -e ':a' -e '$!{N;ba' -e '}' -e 's/[&/\]/\\&/g; s/\n/\\&/g' <<<"$1")
+    printf %s "${REPLY%$'\n'}"
+}
+
 cp ./public_html/boilerplate/template/generic_template.html ./public_html/boilerplate/generic.html;
 cp ./public_html/boilerplate/template/home_template.html ./public_html/boilerplate/home.html;
 
@@ -7,11 +12,15 @@ while read p; do
     
     TAG=$(echo $p | cut -f1 -d-);
     VALUE=$(echo $p | cut -f2 -d-);
-    sed -i -e "s/$TAG/$VALUE/g" public_html/boilerplate/generic.html;
-    sed -i -e "s/$TAG/$VALUE/g" public_html/boilerplate/home.html;
 
-    ##Don't know why I need to do this
-    rm public_html/boilerplate/*.html-e;
+    sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/g" ./public_html/boilerplate/generic.html > temp.html
+    mv temp.html ./public_html/boilerplate/generic.html
+    
+    sed -e ':a' -e '$!{N;ba' -e '}' -e "s/$TAG/$(quoteSubst "$VALUE")/g" public_html/boilerplate/home.html > temp.html;
+    mv temp.html ./public_html/boilerplate/home.html;
+        
 done < $filename
 
 
+cd public_html/builders/;
+make;
