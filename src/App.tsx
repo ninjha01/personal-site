e783f52b011edba66bbbc02a0fcfd23f51892bb6
Nishant <me@nishantjha.org>
2022-05-26 15:39:02 -0400

    google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React, { Suspense } from "react";
+import ReactGA from "react-ga4";
 import { Home } from "./pages/Home";
 
 function App() {
+  const snap = navigator.userAgent !== "ReactSnap";
+  const production = process.env.NODE_ENV === "production";
+  if (production && snap) {
+    ReactGA.initialize("G-EVE1M74Q2V");
+  }
   return (
     <div className="App">
       <Suspense fallback={<div>Loading...</div>}>
