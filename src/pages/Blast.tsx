b1e1834edf4b31af848181e9c43cfc273ff00b28
Nishant <me@nishantjha.org>
2022-05-29 10:41:36 -0400

    steps

--- src/pages/Blast.tsx
+++ src/pages/Blast.tsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { EssaySection } from "../components/EssaySection";
-import { HeroSection } from "../components/HeroSection";
 import { Sidebar } from "../components/Sidebar";
-import { config } from "../config";
+import { Steps, StepType } from "./Step";
 
 export const Blast = () => {
+  const steps: StepType[] = [
+    {
+      name: "Enter Sequence",
+      status: "complete" as const,
+      href: "#",
+      id: "01",
+    },
+    { name: "Blast!", status: "current" as const, href: "#", id: "02" },
+    { name: "Analyze", status: "todo" as const, href: "#", id: "03" },
+  ];
+
   return (
     <>
       <Sidebar>
-        <HeroSection />
-        <EssaySection essays={config.essays} />
+        <Steps steps={steps} />
       </Sidebar>
     </>
   );
---
+++ src/pages/Step.tsx
@@ -0,0 +1,87 @@
+import { CheckIcon } from "@heroicons/react/solid";
+import React from "react";
+
+export interface StepType {
+  name: string;
+  status: string;
+  href: string;
+  id: string;
+}
+export const Steps = (props: { steps: StepType[] }) => {
+  const { steps } = props;
+  return (
+    <nav aria-label="Progress">
+      <ol className="border border-gray-300 rounded-md divide-y divide-gray-300 md:flex md:divide-y-0">
+        {steps.map((step, stepIdx) => (
+          <li key={step.name} className="relative md:flex-1 md:flex">
+            {step.status === "complete" ? (
+              <a href={step.href} className="group flex items-center w-full">
+                <span className="px-6 py-4 flex items-center text-sm font-medium">
+                  <span className="flex-shrink-0 w-10 h-10 flex items-center justify-center bg-blue-900 rounded-full group-hover:bg-blue-800">
+                    <CheckIcon
+                      className="w-6 h-6 text-white"
+                      aria-hidden="true"
+                    />
+                  </span>
+                  <span className="ml-4 text-sm font-medium text-gray-900">
+                    {step.name}
+                  </span>
+                </span>
+              </a>
+            ) : step.status === "current" ? (
+              <a
+                href={step.href}
+                className="px-6 py-4 flex items-center text-sm font-medium"
+                aria-current="step"
+              >
+                <span className="flex-shrink-0 w-10 h-10 flex items-center justify-center border-2 border-blue-900 rounded-full">
+                  <span className="text-blue-900">{step.id}</span>
+                </span>
+                <span className="ml-4 text-sm font-medium text-blue-900">
+                  {step.name}
+                </span>
+              </a>
+            ) : (
+              <a href={step.href} className="group flex items-center">
+                <span className="px-6 py-4 flex items-center text-sm font-medium">
+                  <span className="flex-shrink-0 w-10 h-10 flex items-center justify-center border-2 border-gray-300 rounded-full group-hover:border-gray-400">
+                    <span className="text-gray-500 group-hover:text-gray-900">
+                      {step.id}
+                    </span>
+                  </span>
+                  <span className="ml-4 text-sm font-medium text-gray-500 group-hover:text-gray-900">
+                    {step.name}
+                  </span>
+                </span>
+              </a>
+            )}
+
+            {stepIdx !== steps.length - 1 ? (
+              <>
+                {/* Arrow separator for lg screens and up */}
+                <div
+                  className="hidden md:block absolute top-0 right-0 h-full w-5"
+                  aria-hidden="true"
+                >
+                  <svg
+                    className="h-full w-full text-gray-300"
+                    viewBox="0 0 22 80"
+                    fill="none"
+                    preserveAspectRatio="none"
+                  >
+                    <path
+                      d="M0 -2L20 40L0 82"
+                      vectorEffect="non-scaling-stroke"
+                      stroke="currentcolor"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </div>
+              </>
+            ) : null}
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+};
